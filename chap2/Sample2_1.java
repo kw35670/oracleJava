c3a241f3c643ceeac5b27aa83f8096ab827fc1fa
riku <tkyu58690@gmail.com>
2022-08-29 00:49:48 +0900

    completed chapter2

---
+++ chap2/Sample2_1.java
@@ -0,0 +1,13 @@
+package chap2;
+
+public class Sample2_1 {
+    public static void main(String[] args) {
+        System.out.println(255);
+        System.out.println(0377);
+        System.out.println(0xff);
+        System.out.println(0b101);
+        System.out.println(12.33);
+        System.out.println(3e4);
+        System.out.println('A');
+    }
+}
