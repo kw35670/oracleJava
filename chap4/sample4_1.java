f58dd8d035fbeb87468ed391231a4f8416a0af16
riku <tkyu58690@gmail.com>
2022-08-31 22:14:52 +0900

    completed chap4

---
+++ chap4/sample4_1.java
@@ -0,0 +1,12 @@
+package chap4;
+
+class sample4_1 {
+    public static void main(String[] args) {
+        int num = 0;
+
+        while(num < 5) {
+            System.out.println("numの値は"+ num +"です");
+            num++;
+        }
+    }
+}
