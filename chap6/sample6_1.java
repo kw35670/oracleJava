ba91469e3efa4f020cb209884aca567cdf52d81a
riku <tkyu58690@gmail.com>
2022-09-04 00:23:26 +0900

    completed chap6

---
+++ chap6/sample6_1.java
@@ -0,0 +1,25 @@
+package chap6;
+
+class Employee{
+    int id;
+    void setId(int i) {
+        id = i;
+    }
+
+    int getId() {
+        return id;
+    }
+}
+
+class sample6_1 {
+    public static void main(String[] args) {
+        Employee a = new Employee();
+        a.setId(100);
+
+        Employee b = new Employee();
+        b.setId(200);
+
+        System.out.println(a.getId());
+        System.out.println(b.getId());
+    }
+}
